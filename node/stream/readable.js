24c49ed45f7c51bfd4580c3331445b4b140fad1b
Riophae Lee <riophaelee@gmail.com>
2016-05-11 18:02:51 +0800

    add example for stream.Readable

---
+++ node/stream/readable.js
@@ -0,0 +1,29 @@
+const { Readable } = require('stream')
+
+class ToReadable extends Readable {
+  constructor(iterable) {
+    super()
+    this.iterator = (function* () {
+      yield* iterable
+    }())
+  }
+
+  _read() {
+    const res = this.iterator.next()
+    if (res.done) {
+      this.push(null)
+    } else {
+      this.push(`${res.value}\n`)
+    }
+  }
+}
+
+const iterable = (function* (limit) {
+  while (limit--) { // eslint-disable-line no-param-reassign
+    yield Math.random()
+  }
+}(25))
+
+const readable = new ToReadable(iterable)
+readable.on('data', (data) => process.stdout.write(data))
+readable.on('end', () => process.stdout.write('DONE\n'))
