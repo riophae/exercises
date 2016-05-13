4f53409afa520327abf4cec2f5319cf389d36eab
Riophae Lee <riophaelee@gmail.com>
2016-05-13 18:10:43 +0800

    add example for paused mode

---
+++ node/stream/paused-mode.js
@@ -0,0 +1,22 @@
+const { Readable } = require('stream')
+
+const dataSource = ['a', 'b', 'c']
+const readable = new Readable()
+readable._read = function () {
+  process.nextTick(() => {
+    if (dataSource.length) {
+      this.push(dataSource.shift())
+    } else {
+      this.push(null)
+    }
+  })
+}
+
+readable.pause() // enter paused mode
+
+readable.on('data', (data) => process.stdout.write(`data: ${data}\n`))
+
+// `readable.read()` will be called after `readable` event handler has been attached
+readable.on('readable', () => {
+  readable.read()
+})
