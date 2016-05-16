416da1bea11ab0ce71132f8cee62b521c2d054b7
Riophae Lee <riophaelee@gmail.com>
2016-05-16 20:41:04 +0800

    rename dir

---
+++ node/http/basic.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+http.createServer((req, res) => {
+  let body = ''
+  req
+    .setEncoding('utf8')
+    .on('data', (chunk) => {
+      body += chunk
+    })
+    .on('end', () => {
+      let data
+      try {
+        data = JSON.parse(body)
+      } catch (ex) {
+        res.statusCode = 400
+        res.end(`Error: ${ex.message}`)
+        return
+      }
+
+      res.end(typeof data)
+    })
+}).listen(1337)
