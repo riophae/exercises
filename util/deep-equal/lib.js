a2e28c1648d47340fc55e9e319d5c8c51fe473f8
Riophae Lee <riophaelee@gmail.com>
2016-05-18 21:06:32 +0800

    add a simple implementation of deep-equal

---
+++ util/deep-equal/lib.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-use-before-define */
+
+const objToStr = Object.prototype.toString
+const arrEvery = Array.prototype.every
+
+function getTypeTag(thing) {
+  return objToStr.call(thing).slice(8, -1)
+}
+
+function isArrayLike(thing) {
+  return (
+    thing &&
+    typeof thing === 'object' &&
+    typeof thing.length === 'number'
+  )
+}
+
+function compareArr(arrX, arrY) {
+  if (arrX.length !== arrY.length) return false
+  return arrEvery.call(arrX, (_, idx) => deepEqual(arrX[idx], arrY[idx]))
+}
+
+const deepEqual = module.exports = function deepEqual(value, other) {
+  if (value === other) return true
+  if (value == null) return other == null
+
+  const valueTypeTag = getTypeTag(value)
+  const otherTypeTag = getTypeTag(other)
+  if (valueTypeTag !== otherTypeTag) return false
+
+  if (valueTypeTag === 'Number') {
+    if (Number.isNaN(value)) return Number.isNaN(other)
+    return Number(value) === Number(other)
+  } else if (valueTypeTag === 'String') {
+    return String(value) === String(other)
+  } else if (valueTypeTag === 'Date') {
+    return Date(value) === Date(other)
+  } else if (valueTypeTag === 'Function') {
+    return false
+  } else if (valueTypeTag === 'Array' || isArrayLike(value)) {
+    return compareArr(value, other)
+  } else if (valueTypeTag === 'Object') {
+    const valueKeys = Object.keys(value)
+    const otherKeys = Object.keys(other)
+    if (valueKeys.length !== otherKeys.length) return false
+    return valueKeys.every((key) => deepEqual(value[key], other[key]))
+  }
+
+  return false
+}
---
+++ util/deep-equal/test.js
@@ -0,0 +1,82 @@
+/* eslint-disable */
+
+const test = require('tape')
+const equal = require('./lib')
+
+test('equal', function (t) {
+  t.ok(equal({
+    a: [2, 3],
+    b: [4],
+  }, {
+    a: [2, 3],
+    b: [4],
+  }))
+  t.end()
+})
+
+test('not equal', function (t) {
+  t.notOk(equal({
+    x: 5,
+    y: [6],
+  }, {
+    x: 5,
+    y: 6,
+  }))
+  t.end()
+})
+
+test('nested nulls', function (t) {
+  t.ok(equal([null, null, null], [null, null, null]))
+  t.ok(equal([null, undefined, null], [undefined, null, undefined]))
+  t.end()
+})
+
+test('non-objects', function (t) {
+  t.ok(equal(3, 3))
+  t.ok(equal('beep', 'beep'))
+  t.notOk(equal('3', 3))
+  t.notOk(equal('3', [3]))
+  t.ok(equal(NaN, NaN))
+  t.end()
+})
+
+test('arguments class', function (t) {
+  t.ok(equal(
+        (function () {
+          return arguments
+        })(1, 2, 3), (function () {
+          return arguments
+        })(1, 2, 3),
+        'compares arguments'
+    ))
+  t.notOk(equal(
+        (function () {
+          return arguments
+        })(1, 2, 3), [1, 2, 3],
+        'differenciates array and arguments'
+    ))
+  t.end()
+})
+
+test('dates', function (t) {
+  const d0 = new Date(1387585278000)
+  const d1 = new Date('Fri Dec 20 2013 16:21:18 GMT-0800 (PST)')
+  t.ok(equal(d0, d1))
+  t.end()
+})
+
+test('buffers', function (t) {
+  t.ok(equal(Buffer('xyz'), Buffer('xyz')))
+  t.notOk(equal(Buffer('abc'), Buffer('edf')))
+  t.end()
+})
+
+test('booleans and arrays', function (t) {
+  t.notOk(equal(true, []))
+  t.end()
+})
+
+test('null == undefined', function (t) {
+  t.ok(equal(null, undefined))
+  t.end()
+})
