e4ef3a03ca4b8e54a852ba19c3d046b75f0ec34e
Riophae Lee <riophaelee@gmail.com>
2016-10-04 04:36:00 +0800

    add solution for ex-7.24

---
+++ cpp-primer-5th/ch07/ex-7.24.hpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+
+class Screen  {
+public:
+  using pos = std::string::size_type;
+
+private:
+  pos cursor = 0;
+  pos height = 0, width = 0;
+  std::string contents;
+
+public:
+  Screen() = default;
+  Screen(pos ht, pos wd):
+         height(ht), width(wd), contents(ht * wd, ' ') { }
+  Screen(pos ht, pos wd, char c):
+         height(ht), width(wd), contents(ht * wd, c) { }
+
+  char get() const { return contents[cursor]; }
+  char get(pos ht, pos wd) const;
+  Screen& move(pos r, pos c);
+
+private:
+  pos calc_pos(pos r, pos c) const { return r * width + c; }
+};
+
+inline
+Screen& Screen::move(pos r, pos c) {
+  cursor = calc_pos(r, c);
+  return *this;
+}
+
+inline
+char Screen::get(pos r, pos c) const {
+  return contents[calc_pos(r, c)];
+}
+
+class Window_mgr {
+private:
+  std::vector<Screen> screens{ Screen(24, 80, ' ') };
+};
