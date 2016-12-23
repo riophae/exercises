46c708756cc8db16ce87ecd6b61fba651366fd1c
Riophae Lee <riophaelee@gmail.com>
2016-12-23 20:40:45 +0800

    add heap

---
+++ algorithms-and-data-structures/heap/implementation.js
@@ -0,0 +1,197 @@
+const utils = {
+  getParentIndex(childIdx) {
+    return Math.ceil(childIdx / 2) - 1
+  },
+
+  getChildrenIndex(parentIdx) {
+    const childIdx2 = 2 * (parentIdx + 1)
+    const childIdx1 = childIdx2 - 1
+    return [childIdx1, childIdx2]
+  },
+
+  swap(obj, keyA, keyB) {
+    [obj[keyA], obj[keyB]] = [obj[keyB], obj[keyA]]
+  },
+
+  isLessThan(a, b) {
+    return a < b
+  },
+
+  isGreaterThan(a, b) {
+    return a > b
+  },
+}
+
+class Heap {
+  constructor(comparer, data = []) {
+    this._data = data
+    this._swap = utils.swap.bind(null, this._data)
+    this._comparer = comparer
+  }
+
+  size() {
+    return this._data.length
+  }
+
+  isEmpty() {
+    return this.size() === 0
+  }
+
+  insert(elem) {
+    this._data.push(elem)
+    this._siftUp()
+  }
+
+  replace(elem) {
+    this._data[0] = elem
+    this._siftDown()
+  }
+
+  _peek() {
+    return this._data[0]
+  }
+
+  _removeRootNode() {
+    this._data.shift()
+
+    if (this.size() > 1) {
+      this._data.unshift(this._data.pop())
+      this._siftDown()
+    }
+  }
+
+  _extractRootNode() {
+    const head = this._data[0]
+    this._removeRootNode()
+    return head
+  }
+
+  _checkEmpty(errMsg) {
+    if (!this.size()) {
+      throw new Error(`Unable to ${errMsg}: the heap is empty.`)
+    }
+  }
+
+  _siftUp() {
+    if (this.size() < 2) {
+      return
+    }
+
+    let lightIdx = this.size() - 1
+    let parentIdx = utils.getParentIndex(lightIdx)
+
+    while (parentIdx > -1 && this._comparer(this._data[lightIdx], this._data[parentIdx])) {
+      this._swap(lightIdx, parentIdx)
+      lightIdx = parentIdx
+      parentIdx = utils.getParentIndex(parentIdx)
+    }
+  }
+
+  _siftDown() {
+    if (this.size() < 2) {
+      return
+    }
+
+    let heavyIdx = 0
+    let [childIdx1, childIdx2] = utils.getChildrenIndex(heavyIdx)
+    let lightIdx
+    const idxLimit = this.size() - 1
+
+    while (true) {
+      if (childIdx1 > idxLimit) {
+        break
+      } else if (childIdx2 > idxLimit) {
+        lightIdx = childIdx1
+      } else {
+        lightIdx = this._comparer(this._data[childIdx1], this._data[childIdx2])
+          ? childIdx1
+          : childIdx2
+      }
+
+      if (this._comparer(this._data[lightIdx], this._data[heavyIdx])) {
+        this._swap(lightIdx, heavyIdx)
+        heavyIdx = lightIdx
+        ;[childIdx1, childIdx2] = utils.getChildrenIndex(heavyIdx)
+      } else {
+        break
+      }
+    }
+  }
+}
+
+class MinHeap extends Heap {
+  constructor(data) {
+    super(utils.isLessThan, data)
+  }
+
+  static createHeap() {
+    return new MinHeap()
+  }
+
+  static heapify(arr) {
+    const heap = new MinHeap()
+    arr.forEach(heap.insert.bind(heap))
+    return heap
+  }
+
+  findMin() {
+    this._checkEmpty('find min')
+    return this._peek()
+  }
+
+  deleteMin() {
+    this._checkEmpty('delete min')
+    return this._deleteRootNode()
+  }
+
+  extractMin() {
+    this._checkEmpty('extract min')
+    return this._extractRootNode()
+  }
+
+  merge(anotherHeap) {
+    if (!(anotherHeap instanceof MinHeap)) {
+      throw new Error('The heap provided should be of MinHeap.')
+    }
+
+    const heap = new MinHeap(this._data.slice())
+    anotherHeap._data.forEach(heap.insert.bind(heap))
+    return heap
+  }
+
+  meld(anotherHeap) {
+    if (!(anotherHeap instanceof MinHeap)) {
+      throw new Error('The heap provided should be of MinHeap.')
+    }
+
+    anotherHeap._data.forEach(this.insert.bind(this))
+    return this
+  }
+}
+
+class MaxHeap extends Heap {
+  constructor(data) {
+    super(utils.isGreaterThan, data)
+  }
+
+  findMax() {
+    this._checkEmpty('find max')
+    return this._peek()
+  }
+
+  deleteMax() {
+    this._checkEmpty('delete max')
+    return this._deleteRootNode()
+  }
+
+  extractMax() {
+    this._checkEmpty('extract max')
+    return this._extractRootNode()
+  }
+}
+
+module.exports = {
+  utils,
+  MinHeap,
+  MaxHeap,
+}
---
+++ algorithms-and-data-structures/heap/test.js
@@ -0,0 +1,115 @@
+const { test } = require('tap')
+const { utils, MinHeap, MaxHeap } = require('./implementation')
+
+test('utils', (t) => {
+  t.test('utils.getParentIndex()', (st) => {
+    st.equal(utils.getParentIndex(1), 0)
+    st.equal(utils.getParentIndex(2), 0)
+    st.equal(utils.getParentIndex(3), 1)
+    st.equal(utils.getParentIndex(4), 1)
+    st.equal(utils.getParentIndex(5), 2)
+
+    st.end()
+  })
+
+  t.test('utils.getChildrenIndex()', (st) => {
+    st.deepEqual(utils.getChildrenIndex(0), [1, 2])
+    st.deepEqual(utils.getChildrenIndex(1), [3, 4])
+    st.deepEqual(utils.getChildrenIndex(2), [5, 6])
+
+    st.end()
+  })
+
+  t.test('utils.swap()', (st) => {
+    const obj = { a: 1, b: 2 }
+    const expected = { a: 2, b: 1 }
+    utils.swap(obj, 'a', 'b')
+    st.deepEqual(obj, expected)
+
+    st.end()
+  })
+
+  t.end()
+})
+
+test('MinHeap', (t) => {
+  t.test('normal usage', (st) => {
+    const heap = new MinHeap()
+    st.equal(heap.isEmpty(), true)
+    heap.insert(0)
+    st.equal(heap.isEmpty(), false)
+    st.equal(heap.size(), 1)
+    heap.insert(1)
+    heap.insert(2)
+    st.equal(heap.findMin(), 0)
+    heap.insert(3)
+    heap.insert(4)
+    st.deepEqual(heap._data, [0, 1, 2, 3, 4])
+    st.deepEqual(heap.size(), 5)
+    st.equal(heap.findMin(), 0)
+    st.equal(heap.extractMin(), 0)
+    st.deepEqual(heap.size(), 4)
+    st.deepEqual(heap._data, [1, 3, 2, 4])
+    heap.replace(5)
+    st.deepEqual(heap.size(), 4)
+    st.deepEqual(heap._data, [2, 3, 5, 4])
+
+    st.end()
+  })
+
+  t.test('exceptions', (st) => {
+    const heap = new MinHeap()
+    st.throws(
+      () => heap.findMin(),
+      new Error('Unable to find min: the heap is empty.')
+    )
+    st.throws(
+      () => heap.deleteMin(),
+      new Error('Unable to delete min: the heap is empty.')
+    )
+    st.throws(
+      () => heap.extractMin(),
+      new Error('Unable to extract min: the heap is empty.')
+    )
+
+    st.end()
+  })
+
+  t.test('misc', (st) => {
+    const heap = MinHeap.heapify([5, 4, 3, 2, 1])
+    st.equal(heap.size(), 5)
+    st.equal(heap.findMin(), 1)
+    st.deepEqual(heap._data, [1, 2, 4, 5, 3])
+
+    const anotherHeap = MinHeap.heapify([7, 9, 0, 6])
+    heap.meld(anotherHeap)
+    st.equal(heap.size(), 9)
+    st.deepEqual(heap._data, [0, 2, 1, 5, 3, 4, 6, 7, 9])
+
+    st.end()
+  })
+
+  t.end()
+})
+
+test('MaxHeap', (t) => {
+  t.test('normal usage', (st) => {
+    const heap = new MaxHeap()
+    heap.insert(3)
+    st.equal(heap.size(), 1)
+    heap.insert(5)
+    st.equal(heap.size(), 2)
+    st.equal(heap.findMax(), 5)
+    heap.insert(0)
+    st.equal(heap.size(), 3)
+    st.equal(heap.findMax(), 5)
+    st.deepEqual(heap._data, [5, 3, 0])
+    heap.insert(3) // duplicate element
+    st.equal(heap.size(), 4)
+    st.deepEqual(heap._data, [5, 3, 0, 3])
+
+    st.end()
+  })
+
+  t.end()
+})
