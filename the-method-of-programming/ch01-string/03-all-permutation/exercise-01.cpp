fabb0136f6d77e0e7a23ebe54b276c52e64be355
Riophae Lee <riophaelee@gmail.com>
2017-02-11 06:58:13 +0800

    add solution

---
+++ the-method-of-programming/ch01-string/03-all-permutation/exercise-01.cpp
@@ -0,0 +1,34 @@
+#include <cstring>
+#include <iostream>
+#include <cmath>
+
+void calcAllPermutation(char *str, const int len) {
+  int n = 0;
+  const int max = pow(len, len);
+
+  while (n < max) {
+    int i = n;
+    int j = 0;
+
+    while (i > 0) {
+      const int r = i % len;
+      std::cout << str[r];
+      i = (i - r) / len;
+      ++j;
+    }
+
+    while (j++ < len) {
+      std::cout << str[0];
+    }
+
+    std::cout << std::endl;
+    ++n;
+  }
+}
+
+int main() {
+  char str[] = "abc";
+  calcAllPermutation(str, strlen(str));
+
+  return 0;
+}
