f1fe0dc913d652405334428adafc7c3b064e0e41
Riophae Lee <riophaelee@gmail.com>
2017-03-01 23:39:16 +0800

    update

--- data-structures-and-algorithm-analysis-in-c/ch02/ex-2.14.cpp
+++ data-structures-and-algorithm-analysis-in-c/ch02/ex-2.14.cpp
@@ -5,22 +5,18 @@ void findPrimesLessThan(const int n) {
   int *primes = new int[n + 1];
 
   for (int i = 2; i <= n; ++i) {
-    primes[i] = i;
-  }
-
-  for (int i = 2; i < sqrt(n); ++i) {
     if (primes[i] == -1) {
       continue;
     }
+    std::cout << i << " ";
 
-    int j = i;
-    while ((j += i) <= n) {
-      primes[j] = -1;
+    if (i < sqrt(n)) {
+      int j = i;
+
+      while ((j += i) <= n) {
+        primes[j] = -1;
+      }
     }
-  }
-
-  for (int i = 2; i <= n; ++i) {
-    if (primes[i] != -1) std::cout << i << " ";
   }
   std::cout << std::endl;
 }
