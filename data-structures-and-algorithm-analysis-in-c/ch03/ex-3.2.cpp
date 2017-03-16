89363c27550a0f44c1e2cacdc57899a8ec785289
Riophae Lee <riophaelee@gmail.com>
2017-03-16 17:21:15 +0800

    update

--- data-structures-and-algorithm-analysis-in-c/ch03/ex-3.2.cpp
+++ data-structures-and-algorithm-analysis-in-c/ch03/ex-3.2.cpp
@@ -1,26 +1,32 @@
 #include "linked-list.h"
 #include <iostream>
 
-int main() {
-  const int arrA[] =  { 0, 1, 4, 9, 16 };
-  const int arrB[] =  { 3, 0, 2 };
+void
+PrintLots(List L, List P) {
+  Position Node = P;
 
-  List A = ArrayToList(arrA, sizeof(arrA) / sizeof(arrA[0]));
-  List B = ArrayToList(arrB, sizeof(arrB) / sizeof(arrB[0]));
-
-  Position P = B;
-  while (P != NULL) {
-    Position Q = A;
+  while (Node != NULL) {
+    Position Q = L;
     int i = 0;
 
-    while (i != P->Element) {
+    while (i != Node->Element) {
       Q = Q->Next;
       ++i;
     }
     std::cout << Q->Element << std::endl;
 
-    P = P->Next;
+    Node = Node->Next;
   }
+}
+
+int main() {
+  const int arrA[] =  { 0, 1, 4, 9, 16 };
+  const int arrB[] =  { 3, 0, 2 };
+
+  List L = ArrayToList(arrA, sizeof(arrA) / sizeof(arrA[0]));
+  List P = ArrayToList(arrB, sizeof(arrB) / sizeof(arrB[0]));
+
+  PrintLots(L, P);
 
   return 0;
 }
